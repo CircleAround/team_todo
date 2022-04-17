6f72e53c7ccf600c7281bef8cf782f50d359e868
kabane <kabaneshi@gmail.com>
2022-04-17 17:18:25 +0900

    一部ロジックをTeamモデルにメソッド化

--- app/controllers/teams_controller.js
+++ app/controllers/teams_controller.js
@@ -8,17 +8,7 @@ class TeamsController extends Controller {
   }
 
   async store(req, res) {
-    const team = models.Team.build({
-      name: req.body.name,
-      ownerId: req.user.id
-    });
-    await team.save();
-    const member = models.Member.build({
-      teamId: team.id,
-      userId: req.user.id,
-      role: 1
-    });
-    await member.save();
+    const team = await models.Team.createWithOwner(req.user, req.body);
     await req.flash('info', `新規チーム${team.name}を作成しました`);
     res.redirect(`manager/teams/${team.id}`);
   }
--- app/middlewares/managable_team.js
+++ app/middlewares/managable_team.js
@@ -1,9 +1,10 @@
+const models = require("../models");
+
 module.exports = async function managableTeam(req, res, next) {
-  const member = await req.user.getMember({ where : { teamId: req.params.team, role: 1 } });
-  if ( member.length > 0 ) {
-    return next();
-  } else {
+  const team = await models.Team.findByPk(req.params.team);
+  if (!await team.isManager(req.user)) {
     await req.flash('alert', 'アクセスできません');
     res.redirect('/');
   }
+  return next();
 }; 
